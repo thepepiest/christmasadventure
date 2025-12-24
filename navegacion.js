ed8f598f2cc78497e2f17b59808fa7ea57c1179b
thepepiest <pibipoyito@gmail.com>
2025-12-23 19:37:37 -0500

    Update navegacion.js

--- navegacion.js
+++ navegacion.js
@@ -5,18 +5,217 @@ function mostrarWakeup() {
     window.scrollTo(0, 0);
 }
 
-function mostrarBienvenida() {
-    document.getElementById('wakeup').style.display = 'none';
-    document.getElementById('bienvenida').style.display = 'block';
-    window.scrollTo(0, 0);
-}
-
 function mostrarAlready() {
     document.getElementById('wakeup').style.display = 'none';
     document.getElementById('already').style.display = 'block';
     document.getElementById('christmasa').style.display = 'block';
     window.scrollTo(0, 0);
 }
+
+function mostrarPag3() {
+    document.getElementById('already').style.display = 'none';
+    document.getElementById('pag3').style.display = 'block';
+    document.getElementById('pag3.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarPag4() {
+    document.getElementById('pag3').style.display = 'none';
+    document.getElementById('pag4').style.display = 'block';
+    document.getElementById('pag4.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarBomba() {
+    document.getElementById('pag4').style.display = 'none';
+    document.getElementById('pagbomba').style.display = 'block';
+    document.getElementById('pagbomba.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarBomba2() {
+    document.getElementById('pagbomba').style.display = 'none';
+    document.getElementById('pagbomba2').style.display = 'block';
+    document.getElementById('pagbomba2.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarBomba3() {
+    document.getElementById('pagbomba2').style.display = 'none';
+    document.getElementById('pagbomba3').style.display = 'block';
+    document.getElementById('pagbomba3.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarBomba4() {
+    document.getElementById('pagbomba3').style.display = 'none';
+    document.getElementById('pagbomba4').style.display = 'block';
+    document.getElementById('pagbomba4.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarBadEnding() {
+    document.getElementById('pagbomba4').style.display = 'none';
+    document.getElementById('badending').style.display = 'block';
+    document.getElementById('badending.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarStart() {
+    document.getElementById('badending').style.display = 'none';
+    document.getElementById('pag4').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta() {
+    document.getElementById('pag4').style.display = 'none';
+    document.getElementById('paggalleta').style.display = 'block';
+    document.getElementById('paggalleta.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta2() {
+    document.getElementById('paggalleta').style.display = 'none';
+    document.getElementById('paggalleta2').style.display = 'block';
+    document.getElementById('paggalleta2.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta3() {
+    document.getElementById('paggalleta2').style.display = 'none';
+    document.getElementById('paggalleta3').style.display = 'block';
+    document.getElementById('paggalleta3.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta4() {
+    document.getElementById('paggalleta3').style.display = 'none';
+    document.getElementById('paggalleta4').style.display = 'block';
+    document.getElementById('paggalleta4.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+
+function mostrarGalleta5() {
+    document.getElementById('paggalleta4').style.display = 'none';
+    document.getElementById('paggalleta5').style.display = 'block';
+    document.getElementById('paggalleta5.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta6() {
+    document.getElementById('paggalleta5').style.display = 'none';
+    document.getElementById('paggalleta6').style.display = 'block';
+    document.getElementById('paggalleta6.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta7() {
+    document.getElementById('paggalleta6').style.display = 'none';
+    document.getElementById('paggalleta7').style.display = 'block';
+    document.getElementById('paggalleta7.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarGalleta8() {
+    document.getElementById('paggalleta7').style.display = 'none';
+    document.getElementById('paggalleta8').style.display = 'block';
+    document.getElementById('paggalleta8.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrardecision2() {
+    document.getElementById('paggalleta8').style.display = 'none';
+    document.getElementById('decision').style.display = 'block';
+    document.getElementById('decision.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarPrimerRegalo() {
+    document.getElementById('decision').style.display = 'none';
+    document.getElementById('segundoregalo2').style.display = 'none';
+    document.getElementById('primerregalo').style.display = 'block';
+    document.getElementById('primerregalo.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarAbrir() {
+    document.getElementById('primerregalo').style.display = 'none';
+    document.getElementById('primerregalo2').style.display = 'block';
+    document.getElementById('primerregalo2.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarAbrir2() {
+    document.getElementById('segundoregalo').style.display = 'none';
+    document.getElementById('segundoregalo2').style.display = 'block';
+    document.getElementById('segundoregalo2.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarSegundoRegalo() {
+    document.getElementById('decision').style.display = 'none';
+    document.getElementById('primerregalo2').style.display = 'none';
+    document.getElementById('segundoregalo').style.display = 'block';
+    document.getElementById('segundoregalo.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarByeSanta() {
+    document.getElementById('primerregalo2').style.display = 'none';
+    document.getElementById('segundoregalo2').style.display = 'none';
+    document.getElementById('byesanta').style.display = 'block';
+    document.getElementById('byesanta.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarByeSanta2() {
+    document.getElementById('byesanta').style.display = 'none';
+    document.getElementById('byesanta2').style.display = 'block';
+    document.getElementById('byesanta2.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarByeSanta3() {
+    document.getElementById('byesanta2').style.display = 'none';
+    document.getElementById('byesanta3').style.display = 'block';
+    document.getElementById('byesanta3.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarByeSanta4() {
+    document.getElementById('byesanta3').style.display = 'none';
+    document.getElementById('byesanta4').style.display = 'block';
+    document.getElementById('byesanta4.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarByeSanta5() {
+    document.getElementById('byesanta4').style.display = 'none';
+    document.getElementById('byesanta5').style.display = 'block';
+    document.getElementById('byesanta5.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarByeSanta6() {
+    document.getElementById('byesanta5').style.display = 'none';
+    document.getElementById('byesanta6').style.display = 'block';
+    document.getElementById('byesanta6.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+function mostrarFin() {
+    document.getElementById('byesanta6').style.display = 'none';
+    document.getElementById('final').style.display = 'block';
+    document.getElementById('final.2').style.display = 'block';
+    window.scrollTo(0, 0);
+}
+
+
+
+
+
+
 
 /**
  * @param {string} paginaAMostrar 
